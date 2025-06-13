cda23ac1b0f981309a21a1aa078ae6fc1685d4fb
Anouar1150 <121534824+Anouar1150@users.noreply.github.com>
2025-06-13 10:09:38 +0200

    mise à jour et supp des <=

--- utils.py
+++ utils.py
@@ -102,10 +102,10 @@ def ajuster_niveau_posture_selon_conditions(operations, niveau_posture, high_mov
 
         if posture5_gt6kg:
             niveau_posture = 4
-            posture_explication.append("Ajustement posture : posture 5 ≤10 f/h avec effort >6 kg → posture 4")
+            posture_explication.append("Ajustement posture : posture 5 < 10 f/h avec effort >6 kg → posture 4")
         elif posture5_le6kg:
             niveau_posture = 3
-            posture_explication.append("Ajustement posture : posture 5 ≤10 f/h avec effort ≤6 kg → posture 3")
+            posture_explication.append("Ajustement posture : posture 5 < 10 f/h avec effort < 6 kg → posture 3")
 
     # Facteurs globaux : écrasent si plus contraignants
     if high_movement:
